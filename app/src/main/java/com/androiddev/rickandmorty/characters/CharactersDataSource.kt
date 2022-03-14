5366fa71262996142a7885739a6a4517d1dac928
Mert Akdoğan <mertfalcone01@gmail.com>
2022-03-14 06:40:14 +0300

    8

--- app/src/main/java/com/androiddev/rickandmorty/characters/CharactersDataSource.kt
+++ app/src/main/java/com/androiddev/rickandmorty/characters/CharactersDataSource.kt
@@ -3,10 +3,12 @@ package com.androiddev.rickandmorty.characters
 import androidx.paging.PageKeyedDataSource
 import com.androiddev.rickandmorty.SharedViewModel
 import com.androiddev.rickandmorty.network.response.GetCharacterByIdResponse
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class CharactersDataSource(
-    private val viewModel: SharedViewModel,
+    private val coroutineScope: CoroutineScope,
     private val repository: CharactersRepository
 ): PageKeyedDataSource<Int, GetCharacterByIdResponse>(){
 
@@ -15,6 +17,11 @@ class CharactersDataSource(
         params: LoadInitialParams<Int>,
         callback: LoadInitialCallback<Int, GetCharacterByIdResponse>
     ) {
+        coroutineScope.launch {
+            val characterList = repository.getCharacterList(1)
+            callback.onResult(characterList, null, 2)
+        }
+
 
     }
 
@@ -22,6 +29,10 @@ class CharactersDataSource(
         params: LoadParams<Int>,
         callback: LoadCallback<Int, GetCharacterByIdResponse>
     ) {
+        coroutineScope.launch {
+            val characterList = repository.getCharacterList(params.key)
+            callback.onResult(characterList,  params.key + 1 )
+        }
 
     }
 
